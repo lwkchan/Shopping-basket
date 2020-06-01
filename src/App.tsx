fd17b9510ad58039362c04cdbf3413a1dbc3caa5
lwkchan <laurachan205@gmail.com>
2020-06-01 18:29:21 +0100

    feat: add beans card

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import {
+  Container,
+  Grid,
+  Card,
+  Button,
+  CardContent,
+  CardActions,
+  Typography,
+  CardMedia,
+  makeStyles,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  media: {
+    height: 100,
+  },
+});
 
 function App() {
-  return <Container maxWidth="md">My app will go here</Container>;
+  const classes = useStyles();
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={4}>
+        <Grid item>
+          <Card>
+            <CardMedia
+              className={classes.media}
+              image="https://live.staticflickr.com/7034/6703356745_c99b822322_b.jpg"
+            ></CardMedia>
+            <CardContent>
+              <Typography>Beans</Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained" color="primary">
+                Add to cart
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
