24638e140bf82c588cfc845eb6ed87c4d1e12381
lwkchan <laurachan205@gmail.com>
2020-06-01 17:40:17 +0100

    test: add beans with total test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('user can add three Beans to the basket and see the expected total amount', () => {
+  render(<App />);
+  for (let i = 0; i < 3; i++) {
+    fireEvent.click(screen.getByText('Beans'));
+  }
+
+  fireEvent.click(screen.getByText('Show basket'));
+
+  expect(screen.getAllByText('Beans').length).toEqual(3);
+  expect(screen.getAllByText('0.50'));
+
+  screen.getByText('2.40');
 });
