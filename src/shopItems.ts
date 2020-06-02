aaeee9a6ac4c9fce3bc06d9436f9a2c64a781d6b
lwkchan <laurachan205@gmail.com>
2020-06-02 18:22:19 +0100

    feat: :tada: adds coke to the shop

--- src/shopItems.ts
+++ src/shopItems.ts
@@ -6,6 +6,12 @@ const ItemsForSale: ShopItem[] = [
     price: 0.5,
     image: 'https://live.staticflickr.com/7034/6703356745_c99b822322_b.jpg',
   },
+  {
+    name: 'Coke',
+    price: 0.7,
+    image:
+      'https://cdn.pixabay.com/photo/2014/09/26/19/51/coca-cola-462776_1280.jpg',
+  },
 ];
 
 export default ItemsForSale;
