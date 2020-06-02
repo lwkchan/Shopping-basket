790969ccc8e857fc8f04686e5fa214a508e381ec
lwkchan <laurachan205@gmail.com>
2020-06-02 18:28:01 +0100

    test: add test for the coke special offer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -21,7 +21,7 @@ test('user can add three Beans to the basket and see the expected total amount',
 
   getByText('1.50');
 });
-test('user can add two Cokes to the basket and see the expected total amount', () => {
+test('user can add two Cokes to the basket and see the expected total amount with their savings applied', () => {
   render(<App />);
   for (let i = 0; i < 2; i++) {
     fireEvent.click(screen.getByLabelText('Add Coke to basket'));
@@ -35,10 +35,17 @@ test('user can add two Cokes to the basket and see the expected total amount', (
   const cokeListings = getAllByText('Coke');
   expect(cokeListings).toHaveLength(2);
 
-  const priceListings = getAllByText('0.70');
+  const priceListings = getAllByText('1.00');
   expect(priceListings).toHaveLength(2);
 
-  getByText('1.40');
+  getByText('1.40'); // sub-total
+
+  // user can see savings label and how much was saved
+  getByText('Coke 2 for £1');
+  getByText('-0.40');
+
+  // user can see grand total
+  getByText('1.00');
 });
 test('user can add one Coke and two beans to the basket and see the expected total amount', () => {
   render(<App />);
