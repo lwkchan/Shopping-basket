c57a9133053f0dd755ad7230619e89085b175a96
lwkchan <laurachan205@gmail.com>
2020-06-03 19:33:48 +0100

    chore: add discounts to shop items

--- src/shopItems.ts
+++ src/shopItems.ts
@@ -5,12 +5,22 @@ const ItemsForSale: ShopItem[] = [
     name: 'Beans',
     price: 0.5,
     image: 'https://live.staticflickr.com/7034/6703356745_c99b822322_b.jpg',
+    discount: {
+      quantityToQualify: 3,
+      amountOffPerItemQualified: 0.166666,
+      label: 'Beans 3 for 2',
+    },
   },
   {
     name: 'Coke',
     price: 0.7,
     image:
       'https://cdn.pixabay.com/photo/2014/09/26/19/51/coca-cola-462776_1280.jpg',
+    discount: {
+      quantityToQualify: 2,
+      amountOffPerItemQualified: 0.2,
+      label: 'Coke 2 for £1',
+    },
   },
 ];
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,14 @@
 export type ShopItemInBasket = ShopItem & { uuid: string };
 
+type DiscountInfo = {
+  amountOffPerItemQualified: number;
+  label: string;
+  quantityToQualify: number;
+};
+
 export type ShopItem = {
+  discount?: DiscountInfo;
+  image?: string;
   name: string;
   price: number;
-  image?: string;
 };
