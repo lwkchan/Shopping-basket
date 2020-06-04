0a05a6db1bb0e2e7983b4d185749af6f43f469d6
lwkchan <laurachan205@gmail.com>
2020-06-04 09:29:06 +0100

    test: for multiple diffierent discount rows

--- src/getDiscountsFromItems.test.ts
+++ src/getDiscountsFromItems.test.ts
@@ -1,41 +1,39 @@
 import getDiscountsFromItems from './getDiscountsFromItems';
 
-test('it takes the basket items with discount info and returns and array of total discounts information', () => {
+const beansInBasket = {
+  name: 'Beans',
+  price: 0.5,
+  image: 'https://live.staticflickr.com/7034/6703356745_c99b822322_b.jpg',
+  uuid: 'abc',
+  discount: {
+    quantityToQualify: 3,
+    amountOffPerItemQualified: 0.166666,
+    label: 'Beans 3 for 2',
+  },
+};
+
+test('it takes three beans with a 3 for 2 discount and returns and array of one discount applied', () => {
   const result = getDiscountsFromItems([
-    {
-      name: 'Beans',
-      price: 0.5,
-      image: 'https://live.staticflickr.com/7034/6703356745_c99b822322_b.jpg',
-      uuid: 'abc',
-      discount: {
-        quantityToQualify: 3,
-        amountOffPerItemQualified: 0.166666,
-        label: 'Beans 3 for 2',
-      },
-    },
-    {
-      name: 'Beans',
-      price: 0.5,
-      image: 'https://live.staticflickr.com/7034/6703356745_c99b822322_b.jpg',
-      uuid: 'abc',
-      discount: {
-        quantityToQualify: 3,
-        amountOffPerItemQualified: 0.166666,
-        label: 'Beans 3 for 2',
-      },
-    },
-    {
-      name: 'Beans',
-      price: 0.5,
-      image: 'https://live.staticflickr.com/7034/6703356745_c99b822322_b.jpg',
-      uuid: 'abc',
-      discount: {
-        quantityToQualify: 3,
-        amountOffPerItemQualified: 0.166666,
-        label: 'Beans 3 for 2',
-      },
-    },
+    beansInBasket,
+    beansInBasket,
+    beansInBasket,
   ]);
 
   expect(result).toEqual([{ label: 'Beans 3 for 2', amountOff: 0.5 }]);
+});
+
+test('it takes six beans with a 3 for 2 discount and returns and array of two discounts applied', () => {
+  const result = getDiscountsFromItems([
+    beansInBasket,
+    beansInBasket,
+    beansInBasket,
+    beansInBasket,
+    beansInBasket,
+    beansInBasket,
+  ]);
+
+  expect(result).toEqual([
+    { label: 'Beans 3 for 2', amountOff: 0.5 },
+    { label: 'Beans 3 for 2', amountOff: 0.5 },
+  ]);
 });
